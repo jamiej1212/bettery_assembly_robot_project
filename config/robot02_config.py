6af64c258b63f1630040d3c65d110dcb72d59c90
jamiej1212 <jaydee12347@gmail.com>
2026-07-05 18:55:27 +0900

    :wrench: New Constants Added

--- config/robot02_config.py
+++ config/robot02_config.py
@@ -10,38 +10,38 @@ ROBOT_NAME = "NRMK-Indy7"
 # generate_grid(base, grid_x, grid_y, offset_x, offset_y, num_layers, layer_height)에 그대로 전달
 CELL_STORAGE_GRID_PARAMS = {
     6: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 3,
-        "offset_x": 0.03, "offset_y": 0.03},
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
     8: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 4,
-        "offset_x": 0.03, "offset_y": 0.03},
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
 }
 CELL_INSERT_GRID_PARAMS = {
     6: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 3,
-        "offset_x": 0.03, "offset_y": 0.03},
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
     8: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 4,
-        "offset_x": 0.03, "offset_y": 0.03},
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
+}
+CELL_DISCARD_GRID_PARAMS = {
+    6: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 3,
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
+    8: {"base": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], "grid_x": 2, "grid_y": 4,
+        "offset_x": 0.03, "offset_y": 0.03, "offset_z": 0.05},
 }
 
 # ── 위치 좌표 [x, y, z, rx, ry, rz] (m, deg) ──
 '''
     CV: 컨베이어
-    CC: 배터리 셀 컨테이너
-    CD: 배터리 셀 폐기 라인
     LC: 뚜껑 컨테이너
     LD: 뚜껑 폐기 라인
     DD: 배터리팩 전체 폐기 라인
 '''
-POS_CV_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_CV_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_CC_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_CC_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_CD_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_CD_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_LC_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_LC_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_LD_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_LD_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_DD_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-POS_DD_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+CV_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+CV_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+LC_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+LC_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+LD_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+LD_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+DD_TOP = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+DD_BOTTOM = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
 # ── 그리퍼 ──
 GRIPPER_DO_INDEX = 2 # set_do()에 사용되는 디지털 출력 채널 번호
